72be8f6557f57982aa523acd97afe143bfdc7860
Richard Schmidt <richardnew12511@gmail.com>
2022-04-24 16:53:35 -0500

    setup layout for goal manager

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style='auto' />
+      <View></View>
+      <View></View>
     </View>
   );
 }
